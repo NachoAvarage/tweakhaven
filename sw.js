644d029254bae7692704b1acd721f63f5c7d3b4d
NachoAvarage <miaadadadas@proton.me>
2026-09-15 20:04:10 +0200

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,85 @@
+const CACHE_NAME = "coolkidz-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon.svg"
+];
+
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+                .then(cache => {
+
+                    return cache.addAll(FILES);
+
+                })
+
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys()
+                .then(keys => {
+
+                    return Promise.all(
+
+                        keys
+                            .filter(
+                                key =>
+                                    key !== CACHE_NAME
+                            )
+                            .map(
+                                key =>
+                                    caches.delete(key)
+                            )
+
+                    );
+
+                })
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            caches.match(event.request)
+                .then(cached => {
+
+                    if (cached) {
+                        return cached;
+                    }
+
+                    return fetch(event.request);
+
+                })
+
+        );
+
+    }
+);
